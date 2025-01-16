b55765414f3acb02fd0a15176d1863533fd6b7eb
muhmmadzahidmiah <muhmmadzahidmiah@gmail.com>
2025-01-16 10:09:45 +0600

    Add project files.

---
+++ Configuration/AppServices.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using QuiqBlog.Data;
+
+namespace QuiqBlog.Configuration
+{
+    public static class AppServices
+    {
+        public static void AddDefaultServices(this IServiceCollection
+            servicesCollection,IConfiguration configuration)
+        {
+            servicesCollection.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseSqlServer(
+                        configuration.GetConnectionString("DefaultConnection")));
+            servicesCollection.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+            servicesCollection.AddControllersWithViews();
+            servicesCollection.AddRazorPages();
+        }
+    }
+}
---
+++ ViewComponents/FooterViewComponent.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuiqBlog.ViewComponents
+{
+    public class FooterViewComponent : ViewComponent
+    {
+    }
+}
